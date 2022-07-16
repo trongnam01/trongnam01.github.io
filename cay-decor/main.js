5ea7b0d4fbedac06b513ba91a222a405c49e2194
trongnam01 <101313377+trongnam01@users.noreply.github.com>
2022-07-16 16:19:13 +0700

    post

--- cay-decor/main.js
+++ cay-decor/main.js
@@ -13,25 +13,41 @@ let cart = document.querySelector(".header-cart")
 let deleteCart = document.querySelector(".icon-delete-modal")
 let detaiContent = document.querySelector(".products-detai-content")
 let seeMore = document.querySelector(".see-more")
+let methodBank = document.querySelector(".method-bank")
+let payBank = document.querySelector("#bank")
+let whenReceiving = document.querySelector("#when-receiving")
 
 if(cart) {
-
     cart.onclick = function() {
         document.querySelector(".modal-cart").style.display = "flex"
     }
 }
-deleteCart.onclick = () => {
-    document.querySelector(".modal-cart").style.display = "none"
+if(deleteCart) {
+    deleteCart.onclick = () => {
+        document.querySelector(".modal-cart").style.display = "none"
+    }
 }
 
-
-seeMore.onclick = () => {
-    if(detaiContent.getAttribute("class") === "products-detai-content active") {
-        detaiContent.classList.remove("active")
-        seeMore.innerHTML = ' Xem thêm <i class="fa-solid fa-caret-down"></i> '
-    } else {
-        detaiContent.classList.add("active")
-        seeMore.innerHTML = ' Thu gọn <i class="fa-solid fa-caret-up icon-up"></i> '
+if(seeMore) {
+    seeMore.onclick = () => {
+        if(detaiContent.getAttribute("class") === "products-detai-content active") {
+            detaiContent.classList.remove("active")
+            seeMore.innerHTML = ' Xem thêm <i class="fa-solid fa-caret-down"></i> '
+        } else {
+            detaiContent.classList.add("active")
+            seeMore.innerHTML = ' Thu gọn <i class="fa-solid fa-caret-up icon-up"></i> '
+        }
+        seeMore.classList.toggle("active")
     }
-    seeMore.classList.toggle("active")
+}
+
+if(payBank) {
+    payBank.onclick = () => {
+        methodBank.classList.toggle("active")
+    }
+}
+if(whenReceiving) {
+    whenReceiving.onclick = () => {
+        methodBank.classList.remove("active")
+    }
 }
