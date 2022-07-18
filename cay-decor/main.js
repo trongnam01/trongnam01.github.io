0d7bdd0e3388fbdda7b5eaea2592a8e186b83f09
trongnam01 <101313377+trongnam01@users.noreply.github.com>
2022-07-18 12:31:00 +0700

    .

--- cay-decor/main.js
+++ cay-decor/main.js
@@ -1,36 +1,78 @@
 // 
+let ar = document.querySelectorAll(".card-title")
 
+console.log(ar[0].parentElement.parentElement.parentElement);
+let a = ar[1].parentElement.parentElement.parentElement
 
-
+a.onclick = () => {
+    console.log(a);
+}
 
 
 
 // 
 
+let userlogin = false
+let titleProducts = []
 
 
-let cart = document.querySelector(".header-cart")
-let deleteCart = document.querySelector(".icon-delete-modal")
-let detaiContent = document.querySelector(".products-detai-content")
-let seeMore = document.querySelector(".see-more")
-let methodBank = document.querySelector(".method-bank")
-let payBank = document.querySelector("#bank")
-let whenReceiving = document.querySelector("#when-receiving")
 
-if(cart) {
-    cart.onclick = function() {
+let arrTitlCard = Array.from(document.querySelectorAll(".card-title"))
+
+let formProducts = Array.from(document.querySelectorAll(".card-title"))
+
+
+let cart = document.querySelector(".header-cart")                     // gio hang
+let deleteCart = document.querySelector(".icon-delete-modal")         // xóa modal
+let detaiContent = document.querySelector(".products-detai-content")   // xem thêm 
+let seeMore = document.querySelector(".see-more")                       // xem thêm 
+let methodBank = document.querySelector(".method-bank")                    // hình thức chuỷen
+let payBank = document.querySelector("#bank")                               // stk 
+let whenReceiving = document.querySelector("#when-receiving")               //        
+let footerModal = document.querySelector(".footer-modal button")           //  
+let modaproduct = document.querySelector(".modal-cantainer")
+let modalogin = document.querySelector(".modal-login")
+let login = document.querySelector("#login")
+let informationUser = document.querySelector(".modal-information-user")
+let btnNext = document.querySelector(".next")
+let inputSearch = document.querySelector(".slider-input-search")
+
+let formSearch = document.querySelector(".slider-content-ul")           // thẻ ul search
+let ulSearch = document.querySelector(".slider-content-ul ul")           // thẻ ul search
+let contaiProducts = document.querySelector("#products")           // id product
+let prodCard = document.querySelectorAll(".js-products-card")           // card product
+let tabsproducts = document.querySelector("#products .product-item-tabs ")           // tabs product
+let modalCart = document.querySelector(".modal-cart")               // gio hang
+let btnOder = document.querySelector(".btn-pay-order")              // btn order
+let btnHotline = document.querySelector(".btn-hotline")              // btn order
+
+if (modalCart)
+    modalCart.onclick = (e) => {
+        document.querySelector(".modal-cart").style.display = "none"
+    }
+
+
+// mở modal
+if (cart) {
+    cart.onclick = function () {
         document.querySelector(".modal-cart").style.display = "flex"
+        informationUser.style.display = "none"
+        modalogin.style.display = "none"
+        modaproduct.style.display = "block"
     }
 }
-if(deleteCart) {
+
+// đóng modal
+if (deleteCart) {
     deleteCart.onclick = () => {
         document.querySelector(".modal-cart").style.display = "none"
     }
 }
 
-if(seeMore) {
+// mở text producst detai
+if (seeMore) {
     seeMore.onclick = () => {
-        if(detaiContent.getAttribute("class") === "products-detai-content active") {
+        if (detaiContent.getAttribute("class") === "products-detai-content active") {
             detaiContent.classList.remove("active")
             seeMore.innerHTML = ' Xem thêm <i class="fa-solid fa-caret-down"></i> '
         } else {
@@ -41,13 +83,343 @@ if(seeMore) {
     }
 }
 
-if(payBank) {
+// phương thức mua hàng
+if (payBank) {
     payBank.onclick = () => {
-        methodBank.classList.toggle("active")
+        methodBank.classList.add("active")
     }
 }
-if(whenReceiving) {
+if (whenReceiving) {
     whenReceiving.onclick = () => {
         methodBank.classList.remove("active")
+    }
+}
+
+// if đk chuyến đến trang thanh toán
+
+if (footerModal) {
+
+    footerModal.onclick = () => {
+        if (userlogin) {
+            // khi đã đăng nhập
+
+        } else {
+            modaproduct.style.display = "none"
+            modalogin.style.display = "block"
+
+        }
+    }
+}
+
+
+//  bỏ sự lan tỏa từ thẻ cha
+if (modaproduct)
+    modaproduct.onclick = (e) => {
+        e.stopPropagation()
+    }
+
+if (modalogin)
+    modalogin.onclick = (e) => {
+        e.stopPropagation()
+    }
+if (informationUser)
+    informationUser.onclick = (e) => {
+        e.stopPropagation()
+    }
+
+
+// đăng nhập
+if (login) {
+    login.onclick = (e) => {
+        modalogin.style.display = "none"
+        informationUser.style.display = "block"
+
+    }
+}
+// qua trang thanh toán
+if (btnNext) {
+    btnNext.onclick = (e) => {
+        informationUser.style.display = "none"
+        window.location = "payment.html";
+    }
+}
+
+
+// push titleproduct
+formProducts.forEach((data) => {
+    titleProducts.push(data.innerText)
+})
+
+// check li search
+function checkLiSearch(liSearch) {
+    // thẻ ul search
+    for (let i = 5; i < liSearch.length; i++) {
+        liSearch[i].style.display = "none"
+    }
+
+    for (let i = 0; i < liSearch.length; i++) {
+        liSearch[i].setAttribute("onclick", `selectLi(this)`)
+    }
+}
+
+let copyCard
+// input search
+if (inputSearch) {
+    inputSearch.onkeyup = (e) => {
+        let valueinput = e.target.value
+        let keywCard = []
+        let arrelement = []
+
+        if (valueinput) {
+            arrelement = titleProducts.filter(data => {
+                return data.toLowerCase().replace(/ /g, '').startsWith(valueinput.toLowerCase().replace(/ /g, ''))
+            })
+            arrelement = arrelement.map((data) => {
+                return `<li> ${data} </li> `
+            })
+
+            if (document.querySelector(".js-index")) {
+                document.querySelector("#all-products").style.display = "none"
+                document.querySelector(".js-search").style.display = "block"
+
+                for (let i = 0; i < arrTitlCard.length; i++) {
+                    let keywtitlSearch = arrTitlCard[i].innerText
+
+                    if (keywtitlSearch.toLowerCase().replace(/ /g, '').startsWith(valueinput.toLowerCase().replace(/ /g, ''))) {
+                        let parenkeyw = arrTitlCard[i].parentElement.parentElement.parentElement
+                        let sale = ''
+
+                        if (parenkeyw.querySelector("span.sale-off")) {
+                            sale = parenkeyw.querySelector(".sale-off").innerText
+                        }
+
+                        let objProdSearch = {
+                            title: parenkeyw.querySelector(".card h5.card-title").innerText,
+                            img: parenkeyw.querySelector("img").getAttribute("src"),
+                            price: parenkeyw.querySelector(".price").innerText,
+                            sale: sale,
+                            id: parenkeyw.querySelector(".card").getAttribute("id-data")
+                        }
+                        keywCard.push(objProdSearch)
+                        document.querySelector(".js-search > p").innerHTML = ` <p>Kết quả được từ tìm thấy với từ khóa"<span class="keyword-search">${valueinput}</span>"</p>`
+
+                    }
+                }
+                showCard(keywCard)
+                if (keywCard.length === 0) {
+                    document.querySelector(".js-search > p").innerHTML = ` <p> Không tìm thấy kết quả nào tìm thấy với từ khóa "<span class="keyword-search">${valueinput}</span>"</p>`
+                }
+            }
+
+
+            // trang products
+            if (contaiProducts) {
+                tabsproducts.style.display = "none"
+
+                for (let i = 0; i < prodCard.length; i++) {
+                    let titleProd = prodCard[i].querySelector(".card h5.card-title").innerText
+
+                    if (titleProd.toLowerCase().replace(/ /g, '').indexOf(valueinput) > -1) {
+                        prodCard[i].style.display = ""
+                    } else {
+                        prodCard[i].style.display = "none"
+
+                    }
+                }
+            }
+            // trang index
+            if (formSearch)
+                formSearch.classList.add("active")
+
+            showLiSearch(arrelement)
+            showCard(keywCard)
+
+        } else {
+            //  khi ko onkeyup
+
+            if (formSearch)
+                formSearch.classList.remove("active")
+
+
+            if (prodCard)
+                if (contaiProducts) {
+                    tabsproducts.style.display = "flex"
+                }
+            for (let i = 0; i < prodCard.length; i++) {
+                prodCard[i].style.display = "block"
+            }
+
+            if (document.querySelector(".js-index")) {
+                document.querySelector("#all-products").style.display = "block"
+                document.querySelector(".js-search").style.display = "none"
+            }
+        }
+        let liSearch = document.querySelectorAll(".slider-content-ul li")
+        checkLiSearch(liSearch)
+
+        copyCard = [...keywCard]
+    }
+}
+
+function selectLi(e) {
+    inputSearch.value = e.innerText
+    formSearch.classList.remove("active")
+    inputSearch.focus()
+    let copyData = []
+
+    for (let i = 0; i < copyCard.length; i++) {
+        let textCatdCopy = copyCard[i].title
+        if (textCatdCopy.toLowerCase().replace(/ /g, '').startsWith(e.innerText.toLowerCase().replace(/ /g, ''))) {
+            copyData.push(copyCard[i])
+        } else {
+            console.log(copyCard[i]);
+
+        }
+    }
+    console.log(e.innerText);
+    document.querySelector(".js-search > p").innerHTML = ` <p>Kết quả được từ tìm thấy với từ khóa"<span class="keyword-search">${e.innerText}</span>"</p>`
+    showCard(copyData)
+}
+
+// show li search
+function showLiSearch(data) {
+    let htmlUl;
+    if (!data.length) {
+        valueinput = inputSearch.value
+
+        htmlUl = `<li>${valueinput} </li>`
+    } else {
+        htmlUl = data.join('')
+    }
+    if (ulSearch)
+        ulSearch.innerHTML = htmlUl
+}
+
+function showCard(data) {
+    let contaiShowCard = document.querySelector(".js-search > .row")
+
+    let htmlCard = data.map((el) => {
+        return `
+        <div class="col-12 col-sm-6 col-lg-4">
+            <div class="card" id-data="${el.id}">
+                <img src="${el.img}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${el.title}</h5>
+                    <span class="price">${el.price}</span>
+                </div>
+            </div>
+         </div>
+     `
+    }).join('')
+
+    if (document.querySelector(".js-index"))
+        contaiShowCard.innerHTML = htmlCard
+}
+
+
+function dataCard(keywCard, valueinput) {
+
+    if (document.querySelector(".js-index")) {
+        document.querySelector("#all-products").style.display = "none"
+        document.querySelector(".js-search").style.display = "block"
+
+        for (let i = 0; i < arrTitlCard.length; i++) {
+            let keywtitlSearch = arrTitlCard[i].innerText
+
+            if (keywtitlSearch.toLowerCase().replace(/ /g, '').startsWith(valueinput)) {
+                let parenkeyw = arrTitlCard[i].parentElement.parentElement.parentElement
+                let sale = ''
+
+                if (parenkeyw.querySelector("span.sale-off")) {
+                    sale = parenkeyw.querySelector(".sale-off").innerText
+                }
+
+                let objProdSearch = {
+                    title: parenkeyw.querySelector(".card h5.card-title").innerText,
+                    img: parenkeyw.querySelector("img").getAttribute("src"),
+                    price: parenkeyw.querySelector(".price").innerText,
+                    sale: sale,
+                    id: parenkeyw.querySelector(".card").getAttribute("id-data")
+                }
+                keywCard.push(objProdSearch)
+                document.querySelector(".js-search > p").innerHTML = ` <p>Kết quả được từ tìm thấy với từ khóa"<span class="keyword-search">${valueinput}</span>"</p>`
+
+            } else {
+                document.querySelector(".js-search > p").innerHTML = ` <p> Không tìm thấy kết quả nào tìm thấy với từ khóa "<span class="keyword-search">${valueinput}</span>"</p>`
+            }
+        }
+
+        showCard(keywCard)
+    }
+}
+if(btnOder) {
+    btnOder.onclick = () => {
+        toast({
+            title: 'Thành Công',
+            message: 'Bạn đã mua hàng thành công',
+            typeo: 'success',
+            duration: 3000
+        })
+    
+        btnOder.disabled = true;
+    
+        setTimeout(() => {
+            // window.location = "index.html";
+        }, 3000)
+    }
+}
+
+
+function toast({
+    title = '',
+    message = '',
+    typeo = '',
+    duration = 3000
+}) {
+    const main = document.getElementById('toast')
+
+    if (main) {
+        // tạo thẻ div 
+        const toast = document.createElement('div')
+        const icons = {
+            info: "fa-solid fa-circle-check",
+            warning: "fa-solid fa-circle-check",
+            success: "fa-solid fa-circle-check",
+            error: "fa-solid fa-circle-exclamation"
+        }
+        //  xóa tự động
+        const removeTimeo = setTimeout(function () {
+            main.removeChild(toast)
+        }, duration + 1000)
+
+        //   xóa bằng click
+        toast.onclick = function (e) {
+            if (e.target.closest('.toast__close')) {
+                main.removeChild(toast)
+                clearTimeout(removeTimeo);
+            }
+        }
+
+        const icon = icons[typeo]
+        const delay = (duration / 1000).toFixed(2)
+
+        toast.classList.add("toasts", `toast--${typeo}`)
+        toast.style.animation = `sideInLeft ease 0.3s, fadeOut linear 1s ${delay}s forwards`
+        toast.innerHTML = `
+        <div class="toast__icon">
+            <i class="${icon}"></i>
+        </div>
+        <div class="toast__body">
+            <h3 class="toast__title">${title}</h3>
+            <p class="toast__msg">${message} </p>
+        </div>
+        <div class="toast__close">
+            <i class="fa-solid fa-xmark"></i>
+        </div>
+        `;
+
+        // đưa toast vào thẻ main
+        main.appendChild(toast)
+
     }
 }
